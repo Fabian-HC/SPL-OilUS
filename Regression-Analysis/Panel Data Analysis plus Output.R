c3793e7cb7ec568dddc637f3f75d971ac55e483f
goma0001 <goossenm@hu-berlin.de>
2016-06-14 19:37:59 +0200

    Update Panel Data Analysis plus Output.R

--- Regression-Analysis/Panel Data Analysis plus Output.R
+++ Regression-Analysis/Panel Data Analysis plus Output.R
@@ -28,9 +28,12 @@ data$Date<-as.Date(as.character(data$Date), "%d.%m.%Y")
 data <- pdata.frame(data, index = c("Company", "Date"), drop.index = F, row.names = T) 
 
 #fixed effects model
-fe <- plm(Stock ~ Assets.to.Market.Cap +Net.Income + BV.Equity.to.Market.Cap + Debt.to.Equity+Oil+Gas+Market, model = "within", data=data)
+fe <- plm(Stock ~ Assets.to.Market.Cap +Net.Income + BV.Equity.to.Market.Cap + Debt.to.Equity+Oil+Gas+Market, 
+          model = "within", data=data)
 summary(fe)
-stargazer(fe,title="Oneway (individual) effect Within Model",dep.var.labels=c("Stock return"),covariate.labels=c("Assets over market cap.","Net income","Book value equity over market cap.","Debt over equity","Oil price","Gas price","DJI premium"), out="femodel.LATEX")
+stargazer(fe,title="Oneway (individual) effect Within Model",dep.var.labels=c("Stock return"),
+          covariate.labels=c("Assets over market cap.","Net income","Book value equity over market cap.",
+          "Debt over equity","Oil price","Gas price","DJI premium"), out="femodel.LATEX")
 
 res. <- fe$residuals
 plot(res.)
@@ -38,10 +41,13 @@ plot(res., type="l", lwd=2, col="red", xlab="Date", ylab="residuals")
 acf(res.,20)
 
 #random effects model
-re <- plm(Stock ~ Assets.to.Market.Cap +Net.Income + BV.Equity.to.Market.Cap + Debt.to.Equity+Oil+Gas+Market, model = "random", data=data)
+re <- plm(Stock ~ Assets.to.Market.Cap +Net.Income + BV.Equity.to.Market.Cap + Debt.to.Equity+Oil+Gas+Market, 
+          model = "random", data=data)
 summary(re)
 res.2 <- re$residuals
-stargazer(fe,title="Oneway (individual) effect Random Effect Model (Swamy-Arora's transformation)",dep.var.labels=c("Stock return"),covariate.labels=c("Assets over market cap.","Net income","Book value equity over market cap.","Debt over equity","Oil price","Gas price","DJI premium"), out="remodel.LATEX")
+stargazer(fe,title="Oneway (individual) effect Random Effect Model (Swamy-Arora's transformation)",
+          dep.var.labels=c("Stock return"),covariate.labels=c("Assets over market cap.","Net income",
+          "Book value equity over market cap.","Debt over equity","Oil price","Gas price","DJI premium"), out="remodel.LATEX")
 
 #Hausman test comparing RE and FE
 phtest(fe, re)
@@ -56,17 +62,21 @@ plot(data$Debt.to.Equity)
 
 #test for heteroskedasticity
 library(lmtest)
-bptest(Stock ~ Assets.to.Market.Cap +Net.Income + BV.Equity.to.Market.Cap + Debt.to.Equity+Oil+Gas+Market+factor(Company), data=data, studentize=F)
+bptest(Stock ~ Assets.to.Market.Cap +Net.Income + BV.Equity.to.Market.Cap + Debt.to.Equity+Oil+Gas+Market+factor(Company), 
+      data=data, studentize=F)
 
 #heterosk. and serial corr. consistent coefficient
 coeftest(re, vcovHC(re,method="arellano"))
-stargazer(re,title="Arellano model",dep.var.labels=c("Stock return"),covariate.labels=c("Assets over market cap.","Net income","Book value equity over market cap.","Debt over equity","Oil price","Gas price","DJI premium"), out="arellanomodel.LATEX")
+stargazer(re,title="Arellano model",dep.var.labels=c("Stock return"),covariate.labels=c("Assets over market cap.",
+          "Net income","Book value equity over market cap.","Debt over equity","Oil price","Gas price","DJI premium"), 
+          out="arellanomodel.LATEX")
 
 ## SCC robust significance test, default
 coeftest(re, vcov=vcovSCC)
 
 #GLS
-repggls <- pggls(Stock ~ Assets.to.Market.Cap +Net.Income + BV.Equity.to.Market.Cap + Debt.to.Equity +Market+ Oil+Gas, model = "pooling", data=data)
+repggls <- pggls(Stock ~ Assets.to.Market.Cap +Net.Income + BV.Equity.to.Market.Cap + Debt.to.Equity +Market+ Oil+Gas, 
+                model = "pooling", data=data)
 summary(repggls)
 
 # Bars at top indicate corresponding graph from left to right starting on the bottom row
