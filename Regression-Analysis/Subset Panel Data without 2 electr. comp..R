7d27969d7ae6e3de0ca20d7e68a6ce17ec59e3c3
goma0001 <goossenm@hu-berlin.de>
2016-06-14 19:39:16 +0200

    Update Subset Panel Data without 2 electr. comp..R

--- Regression-Analysis/Subset Panel Data without 2 electr. comp..R
+++ Regression-Analysis/Subset Panel Data without 2 electr. comp..R
@@ -7,11 +7,13 @@ Subsetdata$Date<-as.Date(as.character(Subsetdata$Date), "%d.%m.%Y")
 Subsetdata <- pdata.frame(Subsetdata, index = c("Company", "Date"), drop.index = F, row.names = T)
 
 #fixed effects model
-fe2 <- plm(Stock ~ Assets.to.Market.Cap +Net.Income + BV.Equity.to.Market.Cap + Debt.to.Equity+Oil+Gas+Market+EURUSD, model = "within", data=Subsetdata)
+fe2 <- plm(Stock ~ Assets.to.Market.Cap +Net.Income + BV.Equity.to.Market.Cap + Debt.to.Equity+Oil+Gas+Market+EURUSD, 
+       model = "within", data=Subsetdata)
 summary(fe2)
 
 #random effects model
-re2 <- plm(Stock ~ Assets.to.Market.Cap +Net.Income + BV.Equity.to.Market.Cap + Debt.to.Equity+Oil+Gas+Market+EURUSD, model = "random", data=Subsetdata)
+re2 <- plm(Stock ~ Assets.to.Market.Cap +Net.Income + BV.Equity.to.Market.Cap + Debt.to.Equity+Oil+Gas+Market+EURUSD, 
+       model = "random", data=Subsetdata)
 summary(re2)
 
 #Hausman test comparing RE and FE
