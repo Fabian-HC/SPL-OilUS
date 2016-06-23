c23698ae8414d82bb4b37a236f4933c6eed4b134
goma0001 <goossenm@hu-berlin.de>
2016-06-23 15:45:02 +0200

    Update correlation coef.R

--- Regression-Analysis/correlation coef.R
+++ Regression-Analysis/correlation coef.R
@@ -1,43 +1,30 @@
-library(psych)
-library(Hmisc)
-data2<-data[,c(3:11)]
-rcorr<-rcorr(as.matrix(data2),type="spearman")
-corr<-cor(data[,c(3:11)])
-install.packages("tables")
-stargazer(corr,summary=F)
-cor(data[,c(3:11)])
-corrt<-corr.test(data[,c(3:11)], adjust="none")
-as.matrix(rcorr)
-rcorr(as.matrix(data2),type="spearman")
-
-
 library(xtable)
 
-corstarsl <- function(x){ 
+corstarsl = function(x){ 
   require(Hmisc) 
-  x <- as.matrix(x) 
-  R <- rcorr(x,type="spearman")$r 
-  p <- rcorr(x,type="spearman")$P 
+  x = as.matrix(x) 
+  R = rcorr(x)$r 
+  p = rcorr(x)$P 
   
   ## define notions for significance levels; spacing is important.
-  mystars <- ifelse(p < .001, "***", ifelse(p < .01, "** ", ifelse(p < .05, "* ", " ")))
+  mystars = ifelse(p < .001, "***", ifelse(p < .01, "** ", ifelse(p < .05, "* ", " ")))
   
   ## trunctuate the matrix that holds the correlations to two decimal
-  R <- format(round(cbind(rep(-1.11, ncol(x)), R), 2))[,-1] 
+  R = format(round(cbind(rep(-1.11, ncol(x)), R), 2))[,-1] 
   
   ## build a new matrix that includes the correlations with their apropriate stars 
-  Rnew <- matrix(paste(R, mystars, sep=""), ncol=ncol(x)) 
-  diag(Rnew) <- paste(diag(R), " ", sep="") 
-  rownames(Rnew) <- colnames(x) 
-  colnames(Rnew) <- paste(colnames(x), "", sep="") 
+  Rnew = matrix(paste(R, mystars, sep=""), ncol=ncol(x)) 
+  diag(Rnew) = paste(diag(R), " ", sep="") 
+  rownames(Rnew) = colnames(x) 
+  colnames(Rnew) = paste(colnames(x), "", sep="") 
   
   ## remove upper triangle
-  Rnew <- as.matrix(Rnew)
-  Rnew[upper.tri(Rnew, diag = FALSE)] <- ""
-  Rnew <- as.data.frame(Rnew) 
+  Rnew = as.matrix(Rnew)
+  Rnew[upper.tri(Rnew, diag = FALSE)] = ""
+  Rnew = as.data.frame(Rnew) 
   
   ## remove last column and return the matrix (which is now a data frame)
-  Rnew <- cbind(Rnew[1:length(Rnew)-1])
+  Rnew = cbind(Rnew[1:length(Rnew)-1])
   return(Rnew) 
 }
 corstarsl(data2)
