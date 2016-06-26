6500bf03919492763577c25aad3804859080a255
anttonH <antton@client195-38.wlan.hu-berlin.de>
2016-06-26 17:32:26 +0200

    looking at result with debt in return

--- Quantlet 5/Trans2TIMD/VarTransStation.R
+++ Quantlet 5/Trans2TIMD/VarTransStation.R
@@ -73,13 +73,13 @@ save(data, file="./Data-Set/InitialData_Panel_Date_OK_OLD_ZScore.RData")
 
 # === Apply (Stationarity) Transformations ==
 # Apply log - transformation to variable
-dataHelp = cbind(data[,1:2], log(data[, c(4,6)]), data[,c(18)])
-colnames(dataHelp) = colnames(data[,c(1:2, 4,6, 18)])
+dataHelp = cbind(data[,1:2], log(data[, c(4)]), data[,c(18)])
+colnames(dataHelp) = colnames(data[,c(1:2, 4, 18)])
 # else:  "A-MCAP","D-MCAP"
 
 # Apply log-return transformation to predefinded variable set
 LogR = apply (
-  X = data[,c(3, 8:17)], 
+  X = data[,c(3,6, 8:17)], 
   MARGIN = 2 , 
   logreturnsfun
 )
@@ -103,7 +103,7 @@ rm(H)
 # The correct date, for which no log return can be obtained was deleted
 
 # Final adjustments
-dataFinal = Datatrans2[,c(1,2,6,4,17, 3, 18, 7,8,9:16,5)] # order data as before
+dataFinal = Datatrans2[,c(1,2,4,17,6, 3, 18, 7,8,9:16,5)] # order data as before
 # colnames(dataFinal) = colnames(data) # assign previous column names
 data = dataFinal 
 # remove auxiliary variables / data frames
@@ -116,99 +116,4 @@ data$Market = data$Market - data$T.Bill3M
 data$T.Bill3M = NULL
 
 # Store transformed dataset for regression analysis and graphical analysis
-save(data, file="./Data-Set/For_Marcus_OK_Old_Version.RData")
-
-
-# === Stationarity Tests for transformed data ===
-# (1) Apply panel data unit root test
-object <- as.data.frame(split(data[,3:11], data$Company))
-PanelUnitRootTest = purtest(object = object, test = "levinlin", 
-                            exo = "trend", lags = "AIC", pmax = 5)
-sink(file = "./Stationarity-Tests/Stationarity__Return_Panel_Test.txt")
-PanelUnitRootTest$statistic
-sink()
-rm(object, PanelUnitRootTest)
-# result: According to this test, the variables in our panel are stationary
-
-# Stationarity Test for common factors- ADF
-Sub1 = ComFSep(data)
-testresult = apply(Sub1, MARGIN = 2, FUN = stattestFUN)
-testresult = data.frame(testresult)
-# all values ok - all stationary
-write.csv2(testresult, file = "./Stationarity-Tests/Stationarity_CommonFactors_Returns_ADF.csv") # csv table export
-print.xtable(xtable(testresult, auto = TRUE), file = "./Stationarity-Tests/Stationarity_Test_Returns_CommonFactors_ADF.txt")
-rm(testresult)
-
-# Stationarity Test for common factors- KPSS
-testresult2 = apply(Sub1, MARGIN = 2, FUN = stattestFUN2)
-testresult2 = data.frame(testresult2)
-# Note that H1: non-trend-stationarity of EUR/USD is accepted for EUR/USD at 10%,
-# but not at the 5% level
-write.csv2(testresult2, file = "./Stationarity-Tests/Stationarity_CommonFactors_Returns_KPSS.csv") # csv table export
-print.xtable(xtable(testresult2, auto = TRUE), file = "./Stationarity-Tests/Stationarity_Test_Returns_CommonFactors_KPSS.txt")
-# Dataset Sub1 not needed now - discard it
-rm(Sub1, testresult2)
-
-# Stationarity Test for company-specific factors as returns - ADF Test
-testresult = aggregate(data[,3:7], by = list(data$Company), 
-                       FUN = stattestFUN, simplify = FALSE)
-testresult = testresult[,-1]
-testresult <- do.call("rbind", lapply(testresult, as.data.frame)) 
-colnames(testresult) = levels(data$Company) 
-testresult = as.data.frame(t(testresult))
-write.csv2(testresult, file = "./Stationarity-Tests/Stationarity__Returns_Company_Specific_ADF.csv") # csv table export
-print.xtable(xtable(testresult, auto = TRUE), file = "./Stationarity-Tests/Stationarity_Test_Returns_Company-Specific_ADF.txt")
-
-
-# Stationarity Test for company-specific factors as returns - KPSS Test
-testresult2 = aggregate(data[,3:7], by = list(data$Company), 
-                        FUN = stattestFUN2, simplify = FALSE)
-testresult2 = testresult2[,-1]
-testresult2 <- do.call("rbind", lapply(testresult2, as.data.frame)) 
-colnames(testresult2) = levels(data$Company) 
-testresult2 = as.data.frame(t(testresult2))
-write.csv2(testresult2, file = "./Stationarity-Tests/Stationarity__Returns_Company_Specific_KPSS.csv") # csv table export
-print.xtable(xtable(testresult, auto = TRUE), file = "./Stationarity-Tests/Stationarity_Test_Returns_Company-Specific_KPSS.txt")
-
-rm(list = ls())
-
-
-# ---------------------------------------------------
-# ---------------------------------------------------
-# Old Code Sections, not used
-
-
-#firstdiffun = function(x){
-#  n = length(x)
-#  firstdif = diff(x, lag=1)
-#}
-
-
-
-
-
-
-# Add the variable Assets to our dataset in order to obtain
-# Net Income over Assets: NI / A 
-#CompAssets = read.csv2("./Data-Set/Company_TotAssets.csv", stringsAsFactors = FALSE)
-#CompAssets$Date <- as.Date(CompAssets$Date, format = "%d.%m.%Y")
-#class(CompAssets$Date)
-# If the classes of the Company indicator match - we can continue
-#class(data$Company) == class(CompAssets$Company)
-
-# merge the two datasets
-#data = merge(data, CompAssets)
-#data$Net.Income = data$Net.Income / data$Assets
-#data$Assets = NULL
-#names(data) = gsub("Net.Income", "NI_by_Assets", names(data))
-# remove the dataset "CompAssets" - not necessary anymore
-#rm(CompAssets)
-# Are all variables there that we need (17 variables)?
-#length(colnames(data)) == 17
-
-#first difference
-#FirstDiff = apply (
-#  X = data[,c(5,7)], 
-#  MARGIN = 2 , 
-#  firstdiffun
-#)
+save(data, file="./Quantlet 5/Trans2TIMD/DebtReturn.RData")
