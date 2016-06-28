62ed531cda5c3000bc94e4e453ed058050b979aa
anttonH <antton.haramboure@yahoo.fr>
2016-06-28 10:58:13 +0200

    Delet of old result + Computing presentation result

--- Quantlet 5/Quantlet 5 : Subsample Analysis .R
+++ Quantlet 5/Quantlet 5 : Subsample Analysis .R
@@ -9,20 +9,11 @@ graphics.off()
 getwd()
 
 #load variable
-load("./Data-Set/For_Marcus_OK_Old_Version.RData")
-#data2 = read.csv2("./Data-Set/Dataset-FINALFORPRESENTATION.csv",stringsAsFactors = FALSE)
-#class(data2$Date)
-#data$Date <- as.Date(data2$Date, format = "%d.%m.%Y")
-#class(data2$Date)
+load("./Data-Set/RegressionBase.RData")
 
-data[78,]
-data2[78,]
-class(data$Date)
-#!(data$Company==3 | data2$Company==8),]
 data0 = data[!(data$Company=="CPEnergy" | data$Company=="PG&E_Corp"),]
-#!(data$Company=="CPEnergy" | data$Company=="PG&E_Corp"),]
 class(data$Company)
-#names(data0)[4:7] = c("A.MCAP", "BVE.MCAP", "D.MCAP","NET")
+
 # Install packages if not installed
 libraries = c("stargazer","tseries","plm")
 lapply(libraries, function(x) if (!(x %in% installed.packages())) {
@@ -46,15 +37,13 @@ Post2008 = pdata.frame(subset(data0, Date > '2008-08-31'),
 
 #two regressions
 #pre 2008
-repre2008 = plm(Stock ~ A.MCAP+ NI+ BVE.MCAP
-                + D.MCAP+ Oil + Gas + Market + EURUSD, 
+repre2008 = plm(Stock ~ Oil + Gas + Market + EURUSD, 
                 model = "random", 
                 data=Pre2008)
 summary(repre2008)
 
 #post 2008  
-rePost2008 = plm(Stock ~ A.MCAP+ NI+ BVE.MCAP
-                 + D.MCAP+ Oil + Gas + Market + EURUSD, 
+rePost2008 = plm(Stock ~ Oil + Gas + Market + EURUSD, 
                  model = "random", 
                  data=Post2008)
 summary(rePost2008)
@@ -80,9 +69,7 @@ dataP = pdata.frame(dataP, index = c("Company", "Date"), drop.index = F,
 #Random Effect with Dummy => DumP*D.MCAP
 
 
-DumyReg = plm(Stock ~ A.MCAP+ NI + BVE.MCAP
-              + D.MCAP + Oil + Gas + Market + EURUSD 
-              + DumP*(A.MCAP) + DumP*NI + DumP*(BVE.MCAP)  + #DumP*D.MCAP#
+DumyReg = plm(Stock ~  Oil + Gas + Market + EURUSD + DumP
               + DumP*Oil + DumP*Gas + DumP*Market + DumP*EURUSD, 
               model = "random", 
               data=dataP)
@@ -91,11 +78,7 @@ summary(DumyReg)
 
 stargazer(DumyReg,type="text",
           title="Random Effect Model with Dummy after 2008 (= 1)",
-          dep.var.labels=c("Stock return"),
-          # covariate.labels=c("Assets/market cap.","Net income","BVE/market cap.",
-          "Debt/equity","Oil price","Gas price","DJI premium","EUR/Dollar",
-          "Dummy","Assets/market cap*Dummy","Net income*Dummy","BVE/market cap*Dummy",
-          "Debt/equity*Dummy","Oil price*Dummy","Gas price*Dummy","DJI premium*Dummy","EUR/Dollar*Dummy"), 
+          dep.var.labels=c("Stock return"), 
 out="./Quantlet 5/Regression-Dummy2008.txt")
 
 ###############  SEASONALITY  #############
@@ -114,14 +97,12 @@ dataQ = pdata.frame(dataQ, index = c("Company", "Date"),
                     row.names = T) 
 
 #regression#
-Quarter = plm(Stock ~ A.MCAP+NI+ BVE.MCAP + D.MCAP+ Oil + Gas + Market + EURUSD + Quarter, 
+Quarter = plm(Stock ~ Oil + Gas + Market + EURUSD + Quarter, 
               model = "random",
               data=dataQ)
 stargazer(Quarter,type="text",
           title="Random Effect Model with Quarter Demmy",
           dep.var.labels=c("Stock return"),
-          covariate.labels=c("Assets/market cap.","Net income","BVE/market cap.",
-                             "Debt/equity","Oil price","Gas price", "DJI premium","EUR/Dollar"), 
           out="./Quantlet 5/QuarterDummy.txt")
 
 
@@ -132,22 +113,24 @@ stargazer(Quarter,type="text",
 
 # subsetting #
 # the first subset has already been created #
-OilC = data0
+OilC = pdata.frame(data0, 
+                   index = c("Company", "Date"), 
+                   drop.index = F, 
+                   row.names = T)
+
 OtherC = pdata.frame(subset(data,data$Company == "CPEnergy" | data$Company == "PG&E_Corp"),
                      index = c("Company", "Date"), 
                      drop.index = F, 
                      row.names = T) 
 #Random Effect#
 #OilC
-reOilC = plm(Stock ~ A.MCAP+ NI+ BVE.MCAP
-             + D.MCAP+ Oil + Gas + Market + EURUSD, 
+reOilC = plm(Stock ~  Oil + Gas + Market + EURUSD, 
              model = "random", 
              data=OilC)
 summary(reOilC)
 
 #OtherC  
-reOtherC = plm(Stock ~ A.MCAP+ NI+ BVE.MCAP
-               + D.MCAP+ Oil + Gas + Market + EURUSD, 
+reOtherC = plm(Stock ~ Oil + Gas + Market + EURUSD, 
                model = "random", 
                data=OtherC)
 summary(reOtherC)
@@ -156,8 +139,6 @@ summary(reOtherC)
 stargazer(reOilC, reOtherC, type="text", 
           title="Random Effect Model depending on Company type",
           dep.var.labels=c("Stock return"),
-          covariate.labels=c("Assets/market cap.","Net income","BVE/market cap.",
-                             "Debt/equity","Oil price","Gas price", "DJI premium","EUR/Dollar"), 
           out="./Quantlet 5/Regression.Oil-Other.txt")
 
 ############### Regression with a Dummy ################
@@ -169,19 +150,13 @@ DataT = pdata.frame(DataT, index = c("Company", "Date"),
                     drop.index = F, 
                     row.names = T) 
 
-
-TypeFirm = plm(Stock ~ A.MCAP+NI+ BVE.MCAP
-               + D.MCAP+ Oil + Gas + Market + EURUSD + DumFirmT + DumFirmT*NI+ DumFirmT*BVE.MCAP
-               + DumFirmT*D.MCAP+ DumFirmT*Oil + DumFirmT*Gas + DumFirmT*Market + DumFirmT*EURUSD, 
+TypeFirm = plm(Stock ~  Oil + Gas + Market + EURUSD + DumFirmT + 
+                 DumFirmT*Oil + DumFirmT*Gas + DumFirmT*Market + DumFirmT*EURUSD, 
                model = "random",
                data=DataT)
 stargazer(TypeFirm,type="text",
           title="Random Effect Model with Dummy for Type of firm",
-          dep.var.labels=c("Stock return"),
-          covariate.labels=c("Assets/market cap.","Net income","BVE/market cap.",
-                             "Debt/equity","Oil price","Gas price","EUR/Dollar",
-                             "Dummy","Assets/market cap*Dummy","Net income*Dummy","BVE/market cap*Dummy",
-                             "Debt/equity*Dummy","Oil price*Dummy","Gas price*Dummy","EUR/Dollar*Dummy"), 
+          dep.var.labels=c("Stock return"), 
           out="./Quantlet 5/Regression.DummyOil-Other.txt")
 
 
