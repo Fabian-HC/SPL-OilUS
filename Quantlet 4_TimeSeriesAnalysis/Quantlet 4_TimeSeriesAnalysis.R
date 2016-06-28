a31e59b2130e899a6f29f934a9d6082a8714f475
Marcus G <Marcus G>
2016-06-28 22:35:40 +0200

    LATEX

--- Quantlet 4_TimeSeriesAnalysis/Quantlet 4_TimeSeriesAnalysis.R
+++ Quantlet 4_TimeSeriesAnalysis/Quantlet 4_TimeSeriesAnalysis.R
@@ -1,7 +1,6 @@
-load("./Data-Set/For_Marcus_OK_Old_Version.RData")
+# === Load data and install packages ===
+load("./Data-Set/RegressionBase.RData")
 
-data = Datatrans
-rm(dataFinal)
 # Install packages if not installed
 libraries = c("lmtest","sandwich","dyn","forecast","tseries","aod","foreign","plm","car","stargazer")
 lapply(libraries, function(x) if (!(x %in% installed.packages())) {
@@ -11,52 +10,42 @@ lapply(libraries, function(x) if (!(x %in% installed.packages())) {
 # Load packages
 lapply(libraries, library, quietly = TRUE, character.only = TRUE)
 
+# === Time Series Analysis ===
 # company supplying electricity
 PGECorp = subset(data, data$Company=="PG&E_Corp")
 PGECorp = PGECorp[,-(1:2)]
 
-
 # Since we are working with Time Series now, convert the data to a time series object
 PGECorp = ts(PGECorp,start=c(1996,3), end=c(2015,4),frequency=4)
 
-#linear model
+# Linear model
 lin.model.PGE = lm(Stock ~ A.MCAP +NI + BVE.MCAP + D.MCAP+Oil+Gas+Market, 
                    data=PGECorp)
 summary(lin.model.PGE)                   
 plot(lin.model.PGE)
 
-# output
-stargazer(lin.model.PGE,title="lin.model.PGE",dep.var.labels=c("Stock return"),font.size="tiny",
-          out="./Regression-Analysis/linmodelPGE.LATEX")
-
-#test for autocorrelation
+# Test for autocorrelation
 dwtest(lin.model.PGE)
 
-#Breusch-Pagan Test: Null is homosk.
+# Breusch-Pagan Test: Null is homosk.
 bptest(Stock~A.MCAP +NI + BVE.MCAP + D.MCAP+Oil+Gas+Market, data=PGECorp)
 
-#adjusting for heterosk.
+# Adjusting for heterosk.
 coeftest1 = coeftest(lin.model.PGE, vcovHC)
 summary(coeftest1)
 
-# output
-stargazer(coeftest1,title="lin.model.PGE adj. for heterosk.",dep.var.labels=c("Stock return"), font.size="tiny",out="./Regression-Analysis/coeftest1.LATEX")
-
-# different company from oil sector
+# Different company from oil sector
 ApacheCorp = subset(data, data$Company=="Apache") 
 ApacheCorp<-ApacheCorp[,-(1:2)]
 
 # Since we are working with Time Series now, convert the data to a time series object
 ApacheCorp = ts(ApacheCorp,start=c(1996,3), end=c(2015,4),frequency=4)
 
-# linear model
+# Linear model
 lin.model.Apache = lm(Stock ~ A.MCAP +NI + BVE.MCAP + D.MCAP+Oil+Gas+Market, 
                       data=ApacheCorp)
 summary(lin.model.Apache)                   
 plot(lin.model.Apache)
-
-# output
-stargazer(lin.model.Apache,title="lin.model.Apache",dep.var.labels=c("Stock return"), out="./Regression-Analysis/linmodelApache.LATEX")
 
 # test for autocorrelation
 dwtest(lin.model.Apache)
@@ -64,74 +53,39 @@ dwtest(lin.model.Apache)
 # Breusch-Pagan Test: Null is homosk.(which is the result)
 bptest(Stock~A.MCAP +NI + BVE.MCAP + D.MCAP+Oil+Gas+Market, data=ApacheCorp)
 
-########## old with CSV ############
+# Output for LATEX
+mat1                    = summary(lin.model.Apache)$coefficients
+mat1                    = mat1[, c(1, 4)]
+signif                 = rep("", dim(mat1)[1])
 
-setwd("C:/Users/marcu/Downloads")
+signif[mat1[, 2] < 0.1]  = "*"
+signif[mat1[, 2] < 0.05]  = "**"
+signif[mat1[, 2] < 0.01] = "***"
 
-data <- read.csv2("~/Dataset FINAL(paperstyle).csv", stringsAsFactors=FALSE)
+mat1 = as.data.frame(mat1)
 
-# Install packages if not installed
-libraries = c("lmtest","sandwich","dyn","forecast","tseries","aod","foreign","plm","car","stargazer")
-lapply(libraries, function(x) if (!(x %in% installed.packages())) {
-  install.packages(x)
-})
+mat1[, 2] = signif
 
-# Load packages
-lapply(libraries, library, quietly = TRUE, character.only = TRUE)
+names(mat1) = c("Estimate", "")
 
-# company supplying electricity
-PGECorp <- subset(data, data$Company==8)
-PGECorp<-PGECorp[,-(1:2)]
+mat1[, 1] = round(mat1[, 1], 2)
 
-# Since we are working with Time Series now, convert the data to a time series object
-PGECorp <- ts(PGECorp,start=c(1996,3), end=c(2015,4),frequency=4)
+mat2                    = coeftest1[, c(1, 4)]
+signif                 = rep("", dim(mat2)[1])
 
-#linear model
-lin.model.PGE <- lm(Stock ~ Assets.to.Market.Cap +Net.Income + BV.Equity.to.Market.Cap + Debt.to.Equity+Oil+Gas+Market, 
-                    data=PGECorp)
-summary(lin.model.PGE)                   
-plot(lin.model.PGE)
+signif[mat2[, 2] < 0.1]  = "*"
+signif[mat2[, 2] < 0.05]  = "**"
+signif[mat2[, 2] < 0.01] = "***"
 
-# output
-stargazer(lin.model.PGE,title="lin.model.PGE",dep.var.labels=c("Stock return"),covariate.labels=c("Assets over market cap.",
-          "Net income","Book value equity over market cap.","Debt over equity","Oil price","Gas price","DJI premium"), 
-          out="linmodelPGE.LATEX")
+mat2 = as.data.frame(mat2)
 
-#test for autocorrelation
-dwtest(lin.model.PGE)
+mat2[, 2] = signif
 
-#Breusch-Pagan Test: Null is homosk.
-bptest(Stock~Assets.to.Market.Cap +Net.Income + BV.Equity.to.Market.Cap + Debt.to.Equity+Oil+Gas+Market, data=PGECorp)
+names(mat2) = c("Estimate", "")
 
-#adjusting for heterosk.
-coeftest1<-coeftest(lin.model.PGE, vcovHC)
-summary(coeftest1)
+mat2[, 1] = round(mat2[, 1], 2)
 
-# output
-stargazer(coeftest1,title="lin.model.PGE adj. for heterosk.",dep.var.labels=c("Stock return"),
-          covariate.labels=c("Assets over market cap.","Net income","Book value equity over market cap.",
-          "Debt over equity","Oil price","Gas price","DJI premium"), out="coeftest1.LATEX")
+mat3 = cbind(mat1, mat2)
 
-# different company from oil sector
-ApacheCorp <- subset(data, data$Company==2) 
-ApacheCorp<-ApacheCorp[,-(1:2)]
-
-# Since we are working with Time Series now, convert the data to a time series object
-ApacheCorp <- ts(ApacheCorp,start=c(1996,3), end=c(2015,4),frequency=4)
-
-# linear model
-lin.model.Apache <- lm(Stock ~ Assets.to.Market.Cap +Net.Income + BV.Equity.to.Market.Cap + Debt.to.Equity+Oil+Gas+Market, 
-                      data=ApacheCorp)
-summary(lin.model.Apache)                   
-plot(lin.model.Apache)
-
-# output
-stargazer(lin.model.Apache,title="lin.model.Apache",dep.var.labels=c("Stock return"),
-          covariate.labels=c("Assets over market cap.","Net income","Book value equity over market cap.",
-          "Debt over equity","Oil price","Gas price","DJI premium"), out="linmodelApache.LATEX")
-
-# test for autocorrelation
-dwtest(lin.model.Apache)
-
-# Breusch-Pagan Test: Null is homosk.(which is the result)
-bptest(Stock~Assets.to.Market.Cap +Net.Income + BV.Equity.to.Market.Cap + Debt.to.Equity+Oil+Gas+Market+EURUSD, data=ApacheCorp)
+xtable(mat3)
+print(xtable(mat3), type = "latex", size = "tiny", file = "./Quantlet 4_TimeSeriesAnalysis/timeseries.txt")
